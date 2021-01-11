70915f6941a6e832e6ee32fb323f6a47ac1d021b
zhado <sanrazm@gmail.com>
2021-01-12 03:22:17 +0400

    ის არ დაქომითდა ლმაო

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/zsh
 cd $(dirname $0)
 links='-lncurses'
+flags="-Wno-write-strings"
 
 if [ "$1" = "buildrun" ]; then
-	gcc ./src/main.cpp $links -o out && ./out $2
+	gcc ./src/main.cpp $links $flags -o out && ./out $2
 elif [ "$1" = "debrun" ]; then
-	gcc ./src/main.cpp $links -ggdb -O0 -o deb_out && cgdb -args ./deb_out $2
+	gcc ./src/main.cpp $links $flags -ggdb -O0 -o deb_out && gdb-tmux ./deb_out $2
 elif [ "$1" = "build" ]; then
-	gcc ./src/main.cpp $links -o out $2
+	gcc ./src/main.cpp $links $flags -o out $2
 elif [ "$1" = "debbuild" ]; then
-	gcc ./src/main.cpp $links -ggdb -O0 -o deb_out $2
+	gcc ./src/main.cpp $links $flags -ggdb -O0 -o deb_out $2
 fi
--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,156 @@
 #include <linux/types.h>
 #include <sys/types.h>
 #include <ncurses.h>
+#include <cstdlib>
+#include <string.h>
 typedef unsigned long ul64;
 
 const int max_name_size=100;
 
-struct entry {
+struct log_entry {
 	char* name;
 	char* sub_name;
 	time_t start_time;
 	time_t end_time;
 };
 
-void print_normal_time(time_t* tim){
-	tm* broken_down_time=localtime(tim);
-	fprintf(stdout, "%d/%d/%d %d:%d\n",
+struct log {
+	u_int32_t index;
+	u_int32_t allocated;
+	log_entry* entries;
+};
+
+void print_normal_time(time_t tim){
+	tm* broken_down_time=localtime(&tim);
+	fprintf(stdout, "%d/%d/%d %02d:%02d:%02d",
 			broken_down_time->tm_mday,
 			broken_down_time->tm_mon+1,
 			broken_down_time->tm_year+1900,
 			broken_down_time->tm_hour,
-			broken_down_time->tm_min); 
+			broken_down_time->tm_min,
+			broken_down_time->tm_sec); 
+}
+
+void end_last_entry(log* log_p){
+	log_entry* entry=&log_p->entries[log_p->index-1];
+	if(entry->start_time==0){
+		fprintf(stderr, "entry not started.");
+	}else{
+		entry->end_time=(unsigned long)time(NULL);
+	}
+}
+
+void start_entry(log* log_p, char* name, char* sub_name){
+	if(log_p->index!=0)
+		end_last_entry(log_p);
+	log_entry* entry=&log_p->entries[log_p->index];
+
+	entry->name=(char*)malloc(sizeof(char)*max_name_size);
+	entry->sub_name=(char*)malloc(sizeof(char)*max_name_size);
+	entry->end_time=0;
+
+	entry->start_time=(unsigned long)time(NULL);
+	strcpy(entry->name, name);
+	strcpy(entry->sub_name, sub_name);
+	log_p->index++;
+}
+
+
+void print_logs(log* log_p){
+	for(int i=0;i<log_p->index;i++){
+		log_entry* entry=&log_p->entries[i];
+		print_normal_time(entry->start_time);
+		printf(" - ");
+		if(entry->end_time == 0) 
+			printf("now");
+		else 
+			print_normal_time(entry->end_time);
+		printf(" %s-%s\n",entry->name,entry->sub_name);
+	}
+}
+
+log* load_log(char* file_name){
+	FILE* fp=fopen(file_name,"r");
+	log* a_log=(log*)malloc(sizeof(log));
+	a_log->entries=(log_entry*)malloc(sizeof(log_entry)*200);
+	a_log->allocated=200;
+	char line[200];
+	int line_index=0;
+	while (fgets(line,200,fp)!=0){
+		int quotes[4]={0,0,0,0},index=0;
+		for(int i=0;i<strlen(line);i++){
+			if(line[i]=='"'){
+				quotes[index++]=i;
+			}
+		}
+
+		a_log->entries[line_index].name=(char*)malloc(max_name_size);
+		a_log->entries[line_index].sub_name=(char*)malloc(max_name_size);
+
+		// TODO: sahinelebaa es
+		if(quotes[1]!=0){
+			memcpy(a_log->entries[line_index].name, line+quotes[0]+1,quotes[1]-quotes[0]-1);
+		}
+
+		if(quotes[3]!=0){
+			memcpy(a_log->entries[line_index].sub_name, line+quotes[2]+1,quotes[3]-quotes[2]-1);
+		}
+
+		sscanf(line,"%lu %lu",&a_log->entries[line_index].start_time,&a_log->entries[line_index].end_time);
+		printf("%lu aaa\n",a_log->entries[line_index].start_time);
+
+		line_index++;
+	}
+	a_log->index=line_index;
+
+	fclose(fp);
+	return a_log;
+}
+
+void save_log(log* log_p, char* file_name){
+	FILE* fp=fopen(file_name,"w");
+
+	for(int i=0;i<log_p->index;i++){
+		log_entry* entry=&log_p->entries[i];
+		fprintf(fp, "%lu %lu \"%s\" \"%s\"\n",entry->start_time,entry->end_time,entry->name,entry->sub_name);
+	}
+
+	fclose(fp);
 }
 
 int main(){
 	time_t epoch_time=(unsigned long)time(NULL);
-	//print_normal_time(&epoch_time);
+	log* a_log=(log*)malloc(sizeof(log));
+	a_log->index=0;
+	a_log->entries=(log_entry*)malloc(sizeof(log_entry)*100);
+	a_log->allocated=100;
 
-	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
-
+	char* input=(char*)malloc(100);
+	while(true){
+		printf("> ");
+		memset(input,0,strlen(input));
+		fgets(input,100,stdin);
+		char command[100];
+		sscanf(input,"%s",command);
+		if(strcmp(command, "start") == 0){
+			char name[100];
+			memset(name,0,strlen(name));
+			char subname[100];
+			memset(subname,0,strlen(subname));
+			sscanf(input,"%*s %s %s",name,subname);
+			printf("logged %s da %s\n",name,subname);
+			start_entry(a_log, name, subname);
+		}else if (strcmp(command, "print") == 0){
+			print_logs(a_log);
+		}else if (strcmp(command, "save") == 0){
+			save_log(a_log, "cod");
+		}else if (strcmp(command, "load") == 0){
+			a_log=load_log("cod");
+		}else if (strcmp(command, "exit") == 0){
+			printf("saving...");
+			//save_log(a_log, "cod");
+			break;
+		}
+	}
 	return 0;
-	
 }
