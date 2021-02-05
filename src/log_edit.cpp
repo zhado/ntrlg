696a40a5db1519d2206f6847d704b3e047849b14
zhado <sanrazm@gmail.com>
2021-02-05 23:42:09 +0400

    cleanup log edit code

--- src/log_edit.cpp
+++ src/log_edit.cpp
@@ -38,44 +38,28 @@ int log_edit(log_edit_buffer* buffer, int chr){
 
 
 	if(chr > 31 && chr <=126){
-		if(!only_tag_str){
-			if( last_char(name)!=10 && strlen(name) < MAX_NAME_SIZE -2){
-				add_chr_in_str(chr, name, buffer->local_curs_pos, MAX_NAME_SIZE);
-				buffer->local_curs_pos++;
-			}else if ((last_char(name)==10 ) && strlen(tag_str) < MAX_NAME_SIZE-2 && chr !=32){
-				add_chr_in_str(chr, tag_str, buffer->local_curs_pos, MAX_NAME_SIZE);
-				buffer->local_curs_pos++;
-			}
-		}else{
-			if (strlen(tag_str) < MAX_NAME_SIZE -2 && chr !=32){
-				add_chr_in_str(chr, tag_str, buffer->local_curs_pos, MAX_NAME_SIZE);
-				buffer->local_curs_pos++;
-			}
+		if( last_char(name)!=10 && strlen(name) < MAX_NAME_SIZE -2 && !only_tag_str){
+			add_chr_in_str(chr, name, buffer->local_curs_pos, MAX_NAME_SIZE);
+			buffer->local_curs_pos++;
+		}else if (strlen(tag_str) < MAX_NAME_SIZE-2 && chr !=32){
+			add_chr_in_str(chr, tag_str, buffer->local_curs_pos, MAX_NAME_SIZE);
+			buffer->local_curs_pos++;
 		}
 		*autocomp_selection=-1;
 	}else if (chr == 263 || chr==127){
-		if(!only_tag_str){
-			if(last_char(name)!=10 ){
-				int name_len=strlen(name);
-				if(name_len>0 && buffer->local_curs_pos>0){
-					memcpy(name-1+buffer->local_curs_pos, name+buffer->local_curs_pos,MAX_NAME_SIZE-1-buffer->local_curs_pos);
-					buffer->local_curs_pos--;
-				}
-			}else{
-				int tag_len=strlen(tag_str);
-				if(tag_len>0 && buffer->local_curs_pos>0){
-					memcpy(tag_str-1+buffer->local_curs_pos, tag_str+buffer->local_curs_pos,MAX_NAME_SIZE-1-buffer->local_curs_pos);
-					buffer->local_curs_pos--;
-				}
+		if(last_char(name)!=10 && !only_tag_str){
+			int name_len=strlen(name);
+			if(name_len>0 && buffer->local_curs_pos>0){
+				memcpy(name-1+buffer->local_curs_pos, name+buffer->local_curs_pos,MAX_NAME_SIZE-1-buffer->local_curs_pos);
+				buffer->local_curs_pos--;
 			}
 		}else{
-				int tag_len=strlen(tag_str);
-				if(tag_len>0 && buffer->local_curs_pos>0){
-					memcpy(tag_str-1+buffer->local_curs_pos, tag_str+buffer->local_curs_pos,MAX_NAME_SIZE-1-buffer->local_curs_pos);
-					buffer->local_curs_pos--;
-				}
+			int tag_len=strlen(tag_str);
+			if(tag_len>0 && buffer->local_curs_pos>0){
+				memcpy(tag_str-1+buffer->local_curs_pos, tag_str+buffer->local_curs_pos,MAX_NAME_SIZE-1-buffer->local_curs_pos);
+				buffer->local_curs_pos--;
+			}
 		}
-
 	}else if (chr == KEY_UP && (last_char(name)==10 || only_tag_str) ){
 		if(*autocomp_selection< buffer->matched_count-1)
 			*autocomp_selection=*autocomp_selection+1;
@@ -91,6 +75,7 @@ int log_edit(log_edit_buffer* buffer, int chr){
 	}else if (chr == KEY_LEFT && buffer->local_curs_pos>0){
 			buffer->local_curs_pos--;
 	}else if (chr == 10){
+		int tag_str_len=strlen(tag_str);
 		if(last_char(name)!=10 && !only_tag_str){
 			name[strlen(name)]=10;
 			buffer->local_curs_pos=strlen(buffer->sub_name);
@@ -101,7 +86,7 @@ int log_edit(log_edit_buffer* buffer, int chr){
 				return 0;
 			}else{
 				if(get_after_last_comma(tag_str)!=tag_str || only_tag_str)
-					memcpy(tag_str+strlen(tag_str)-strlen(get_after_last_comma(tag_str)+1),
+					memcpy(tag_str+tag_str_len-strlen(get_after_last_comma(tag_str))+1*(tag_str[strlen(tag_str)-1]==32),
 							requested_str,requested_str_size);
 				else
 					memcpy(tag_str+strlen(tag_str)-strlen(get_after_last_comma(tag_str)),
