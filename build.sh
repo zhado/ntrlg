eaaec4c8f3f5e9f483a64309d45611d251e8ef8b
zhado <sanrazm@gmail.com>
2021-02-25 17:46:32 +0400

    updat cod and change build script now we can build with optimizations turned on!

--- build.sh
+++ build.sh
@@ -14,13 +14,27 @@ if [ "$1" = "buildrun" ]; then
 elif [ "$1" = "buildrunpipe" ]; then
 	gcc ./src/main.cpp $links $flags -o out && ./out $2 2> ./errpipe
 elif [ "$1" = "debrun" ]; then
-	gcc ./src/main.cpp $links $flags -ggdb -O0 -o deb_out && gdb ./deb_out $2
+	gcc ./src/main.cpp $links $flags -ggdb3 -O0 -o deb_out && gdb ./deb_out $2
 elif [ "$1" = "build" ]; then
 	gcc ./src/main.cpp $links $flags -o out $2
 elif [ "$1" = "debbuild" ]; then
-	gcc ./src/main.cpp $links $flags -ggdb -O0 -o deb_out $2
+	gcc ./src/main.cpp $links $flags -ggdb3 -O0 -o deb_out $2
 elif [ "$1" = "buildrunfast" ]; then
-	gcc ./src/main.cpp $links $flags -O3 -o out && ./out $2
+	if [ "$(echo $ANDROID_DATA)" != "" ]; then
+		echo "on android"
+		sed -i '/my_port 1901/a ip 192.168.50.103' serv_conf 
+		gcc ./src/main.cpp $links $flags -O3 -D ANDROID -o out && ./out $2
+	else
+		gcc ./src/main.cpp $links $flags -O3 -o out && ./out $2
+	fi
+elif [ "$1" = "buildrunfastdeb" ]; then
+	if [ "$(echo $ANDROID_DATA)" != "" ]; then
+		echo "on android"
+		sed -i '/my_port 1901/a ip 192.168.50.103' serv_conf 
+		gcc ./src/main.cpp $links $flags -O3 -ggdb3 -D ANDROID -o deb_out && gdb ./deb_out $2
+	else
+		gcc ./src/main.cpp $links $flags -O3 -ggdb3 -o deb_out && gdb ./deb_out $2
+	fi
 else 
 	echo "unknown option"
 	exit 1
