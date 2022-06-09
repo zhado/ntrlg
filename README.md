26899812301d07ba3f83c728d0c03cd37aa26e75
zhado <sanrazm@gmail.com>
2022-06-09 18:23:18 +0400

    add info to readme

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # ntrlg
 Terminal time tracker
+
+# Building
+Run:
+```console
+./build.sh build
+```
+
+# Usage
+
+use arrow keys to move forwards and backwards in time. `Up`/`Down` move 'cursor' by zoom granularity, while `Left` and `Right` advance cursor by 24 hour increments.
+
+`l` add a new log at current time
+
+`a` add a new log which starts at last logs end and is still active.
+
+`d` drag start/end/body of an entry
+
+`z` zoom in
+
+`x` zoom out
+
+`D` delete an entry
+
+`s` save
+
+`q` quit
+
+`v` switch to view mode
+
+`w` switch to week mode.
+
+# log editing mode
+
+While in log mode, enter name and tags for current activity/log. Separate tags with commas. Spaces are not allowed. If available, use arrow keys
+to select autocomplete result and press Enter.
+
+# tag mode
+
+press `g` in view mode to enter tag mode.
+
+you will see a list of tags which are being tracked.
+
+press `a` to add a new tag to tracked list.
+While adding it is also possible to specify background color for a tag. For example, `walking(22)` will be green. 
+Numbers for colors can be seen [here](https://www.ditig.com/256-colors-cheat-sheet).
+
+`d` to drag tag up or down.
+
+`Delete` to delete.
+
+use `v` to switch to view mode
+
+
--- build.sh
+++ build.sh
@@ -3,35 +3,36 @@ cd $(dirname $0)
 links='-lncurses -lstdc++ -pthread -lm'
 flags="-Wall"
 COMPILTER="clang"
+OUT="ntrlg"
 
 if [ "$1" = "buildrun" ]; then
 	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-		$COMPILTER ./src/main.c $links $flags -o out && ./out $2
+		$COMPILTER ./src/main.c $links $flags -o $OUT && ./$OUT $2
 	elif [[ "$OSTYPE" == "darwin"* ]]; then
-		$COMPILTER ./src/main.c $links $flags -D MAC_OS -o out && ./out $2
+		$COMPILTER ./src/main.c $links $flags -D MAC_OS -o $OUT && ./$OUT $2
 	elif [[ "$OSTYPE" == "linux-android" ]]; then
 		echo "on android"
 		sed -i '/my_port 1901/a ip 192.168.50.103' serv_conf 
-		$COMPILTER ./src/main.c $links $flags -D ANDROID -o out && ./out $2
+		$COMPILTER ./src/main.c $links $flags -D ANDROID -o $OUT && ./$OUT $2
 	else
 		echo "OS not supported"
 	fi
 
 elif [ "$1" = "buildrunpipe" ]; then
-	$COMPILTER ./src/main.c $links $flags -o out && ./out $2 2> ./errpipe
+	$COMPILTER ./src/main.c $links $flags -o $OUT && ./out $2 2> ./errpipe
 elif [ "$1" = "debrun" ]; then
 	$COMPILTER ./src/main.c $links $flags -ggdb3 -O0 -o deb_out && gdb ./deb_out $2
 elif [ "$1" = "build" ]; then
-	$COMPILTER ./src/main.c $links $flags -o out $2
+	$COMPILTER ./src/main.c $links $flags -o $OUT $2
 elif [ "$1" = "debbuild" ]; then
 	$COMPILTER ./src/main.c $links $flags -ggdb3 -O0 -o deb_out $2
 elif [ "$1" = "buildrunfast" ]; then
 	if [ "$(echo $ANDROID_DATA)" != "" ]; then
 		echo "on android"
 		sed -i '/my_port 1901/a ip 192.168.50.103' serv_conf 
-		$COMPILTER ./src/main.c $links $flags -O3 -D ANDROID -o out && ./out $2
+		$COMPILTER ./src/main.c $links $flags -O3 -D ANDROID -o $OUT && ./out $2
 	else
-		$COMPILTER ./src/main.c $links $flags -O3 -o out && ./out $2
+		$COMPILTER ./src/main.c $links $flags -O3 -o $OUT && ./out $2
 	fi
 elif [ "$1" = "buildrunfastdeb" ]; then
 	if [ "$(echo $ANDROID_DATA)" != "" ]; then
